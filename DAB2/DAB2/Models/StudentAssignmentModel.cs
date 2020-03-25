732dc3a7cdc9afaca095aa824609daf6ad452c68
victorkildahlpetersen <au600963@uni.au.dk>
2020-03-25 14:04:36 +0100

    models

---
+++ DAB2/DAB2/Models/StudentAssignmentModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace DAB2.Models
+{
+    public class StudentAssignmentModel
+    {
+        // DEFINE SELF
+        public int StudentAssignmentModelId { get; set; } //Key
+        public string AuId { get; set; }
+        public string AssignmentId { get; set; }
+
+        // RELATIONS
+        public StudentModel Students { get; set; }
+        public AssignmentModel Assignments { get; set; }
+    }
+}
---
+++ DAB2/DAB2/Models/StudentCourseModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace DAB2.Models
+{
+    public class StudentCourseModel
+    {
+        // DEFINE SELF
+        public int StudentCourseModelId { get; set; } //Key
+        public string AuId { get; set; }
+        public string CourseId { get; set; }
+        public string Semester { get; set; }
+        public string Active { get; set; }
+
+        // RELATIONS
+        public StudentModel Students { get; set; }
+        public CourseModel Courses { get; set; }
+    }
+}
+
+
+
+
+
+//Modelbuilder.Entity<StudentCourseModel>().HasOne=>(scm=>scm.Students).WithMany(sm=>sm.StudentCourses).HasForeignKey=>(scm=>scm.AuId)
